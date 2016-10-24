1d9e57da3bd24f50800386d5240a6dc78d04f6fb
Jose Manuel Aguilera <lol_jma@hotmail.com>
2016-10-24 16:38:42 -0300

    Adding initial migration por visualization. Adding Transantiago time periods to database.

--- visualization/settings.py
+++ visualization/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visualization',
     'carrier.apps.CarrierConfig',
     'AndroidRequests.apps.AndroidRequestsConfig',
     'django.contrib.admin',
