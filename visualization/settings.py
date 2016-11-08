0ef2b0cdaffc7d8e6f59b0ae6ac80b7793188753
Nacho <phfollert@gmail.com>
2016-11-08 12:56:17 -0300

    deleting custom user

--- visualization/settings.py
+++ visualization/settings.py
@@ -79,8 +79,6 @@ DATABASES = database.DATABASES
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
