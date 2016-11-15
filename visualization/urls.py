c70016851c9b48f7a4bb403c4279b76b502697e6
Pato285 <pisbej@gmail.com>
2016-11-15 10:05:17 -0300

    view report images

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,4 +11,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
