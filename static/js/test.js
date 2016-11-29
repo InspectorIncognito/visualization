0230920b96b6a9f2dcab613a830e27638caa1967
Pato285 <pisbej@gmail.com>
2016-11-29 14:23:05 -0300

    add summary chart

---
+++ static/js/test.js
@@ -0,0 +1,53 @@
+/**
+ * Created by patricio on 11/28/16.
+ */
+var url = 'http://' + location.host + '/carriers/getCount/';
+var reports;
+var resumen;
+var detalle;
+$.getJSON(url)
+    .done(function (data) {
+        console.log(data)
+        reports = data;
+        makechart()
+    });
+
+function makechart() {
+    resumen = c3.generate({
+        bindto: '#resumen',
+        data: {
+            // iris data from R
+            json: reports.datatype,
+            type: 'pie',
+            onclick: function (d, i) {
+                makedetail(d.id);
+            }
+        },
+        tooltip: {
+            format: {
+                value: function (x) {
+                    return x;
+                }
+            }
+        }
+    });
+}
+function makedetail(type) {
+    $("#detailtext").text(type);
+    detalle = c3.generate({
+        bindto: '#detalle',
+        data: {
+            // iris data from R
+            json: reports.groups[type],
+            type: 'pie'
+        },
+        tooltip: {
+            format: {
+                value: function (x) {
+                    return x;
+                }
+            }
+        }
+    });
+}
+
