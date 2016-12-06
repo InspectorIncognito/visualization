e1349003631d019a1ae41601a5a9d7cb9f8d7b36
Nacho <phfollert@gmail.com>
2016-12-06 15:48:28 -0300

    responsive true

--- static/js/driversTable.js
+++ static/js/driversTable.js
@@ -7,6 +7,7 @@ var table;
             table = $('#example').dataTable({
                 scrollX: true,
                 pageLength: 15,
+                responsive: true,
                 order: [[1, "desc"]],
                 dom: 'Bfrtip',
                 buttons: [
