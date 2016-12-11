d09864a92581c88c423d19c0305446584fe61944
Nacho <phfollert@gmail.com>
2016-12-11 17:00:29 -0300

    trying color red

--- static/js/reports.js
+++ static/js/reports.js
@@ -51,7 +51,7 @@ function init() {
                         return '<button type="button" class="btn-xs btn-primary" onclick="openModal(\'' + row.imageName + '\')"> Ver imagen </button>';
                     }
                     else
-                        return "No hay Imagen";
+                        return "<p style='color:red'> No hay imagen </p>";
                 }
             },
         ],
