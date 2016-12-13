0f51c5e5b89c7111bd3f91624448f621a3fa4ee6
seanorar <seba_orme@hotmail.com>
2016-12-13 09:53:26 -0300

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -14,25 +14,35 @@ import_data=true
 #####################################################################
 
 if $install_packages; then
-  sudo apt-get update
-  #install python and pip
-  sudo apt-get install python-pip python-dev libpq-dev
-  #install django
-  pip install django psycopg2
-  #install apache
-  sudo apt-get install apache2
-  #install postgres
-  sudo apt-get install postgresql postgresql-contrib
-  #install npm
-  sudo apt-get install nodejs
-  sudo apt-get install npm
-  #install bower
-  sudo npm install -g bower
-  bower install
-  #install postgis
-  sudo apt-get install postgis
-  #install gdal
-  sudo apt-get install binutils libproj-dev gdal-bin
+    # Install all necesary things
+    # use eog to view image through ssh by enabling the -X flag
+    # Ejample: ssh -X .....
+    # then run eog <image>
+    # and wait 
+    sudo apt-get update
+    # install python and pip
+    sudo apt-get --yes install python-pip python-dev libpq-dev
+    # install django
+    pip install -U Django
+    # install postgres
+    sudo apt-get --yes install postgresql postgresql-contrib
+    # install npm
+    sudo apt-get --yes install nodejs
+    sudo apt-get --yes install npm
+    sudo ln -s /usr/bin/nodejs /usr/bin/node
+    # install bower
+    sudo npm install -g bower
+    sudo bower install --allow-root
+    # install postgis
+    sudo apt-get install postgis
+    # install gdal
+    sudo apt-get install --yes binutils libproj-dev gdal-bin
+    sudo apt-get install --yes openssh-server
+    pip install django-crontab
+    pip install psycopg2
+    pip install pytz
+    # install apache
+    sudo apt-get install --yes apache2 libapache2-mod-wsgi
 fi
 
 
