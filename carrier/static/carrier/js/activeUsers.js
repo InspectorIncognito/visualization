99c8ec6b052b403a4d4aac992e1649d6479a8522
Matías Pavez <matias.pavez.b@gmail.com>
2017-01-30 14:43:53 -0300

    cleaned baseline for active users graph

--- carrier/static/carrier/js/activeUsers.js
+++ carrier/static/carrier/js/activeUsers.js
@@ -1,55 +1,16 @@
 $(function () {
-    $('#date_init').datetimepicker({
+    $('#date_filter').datetimepicker({
         defaultDate: moment().subtract(3, 'months'),
+        // defaultDate: moment(),
         format: 'LL'
-    });
-    $('#date_end').datetimepicker({
-        defaultDate: moment(),
-        format: 'LL'
-    });
-    /*$('#hour1').datetimepicker({
-     defaultDate: moment().set({'hour': 0, 'minute': 0}),
-     format: 'LT'
-     });
-     $('#hour2').datetimepicker({
-     defaultDate: moment().set({'hour': 23, 'minute': 59}),
-     format: 'LT'
-     });*/
-    $("#group :input").change(function () {
-        updatechart();
-    });
-    $("#service").change(function () {
-        myFunction(true);
-    });
-    $("#plate").change(function () {
-        myFunction(false);
     });
     $("#filters").on("dp.change", function (e) {
         myFunction(true);
     });
-    $('#service').on("select2:select", function () {
-        if ($(".select2_multiple").val()[0] == "Todos los recorridos") {
-            $("#service").select2("val", "");
-        }
-    });
-    $('#plate').on("select2:select", function () {
-        if ($(".select2_plate").val()[0] == "Todas las patentes") {
-            $("#plate").select2("val", "");
-        }
-    });
     myFunction(true);
 });
-$(document).ready(function () {
-    $(".select2_multiple").select2({
-        placeholder: "Todos los recorridos",
-        allowClear: true
-    });
-    $(".select2_plate").select2({
-        placeholder: "Todas las patentes",
-        allowClear: true
-    });
 
-});
+
 var resp = null;
 var types = 0;
 var chartdata = {
@@ -80,269 +41,10 @@ function reloadchart() {
 
 function updatechart() {
     if (resp != null) {
-        switch ($('input:checked', '#group').val()) {
-            case "weekday":
-                if (chartdata['weekday'] == null) {
-                    chartdata['weekday'] = [];
-                    for (i = 0; i < types.length; i++) {
-                        chartdata['weekday'].push([0, 0, 0, 0, 0, 0, 0]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var day = (moment(resp[i]['timeCreation'], "DD-MM-YYYY HH:mm:SS").day() + 6) % 7;
-                        chartdata['weekday'][type][day] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(chartdata['weekday'][i]));
-                }
-                makechart(cols, ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'], null, null, null, [], 'category', null)
-                break;
 
-            case "daytype":
-                if (chartdata['daytype'] == null) {
-                    chartdata['daytype'] = {
-                        'daytypes': [],
-                        'daytypetype': []
-                    }
-                    chartdata['daytype']['daytypes'] = [];
-                    chartdata['daytype']['daytypetype'] = [];
-                    for (i = 0; i < types.length; i++) {
-                        chartdata['daytype']['daytypetype'].push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var daytype = resp[i]['typeOfDay'];
-                        if (chartdata['daytype']['daytypes'].indexOf(daytype) == -1) {
-                            chartdata['daytype']['daytypes'].push(daytype)
-                            for (var j = 0; j < types.length; j++) {
-                                chartdata['daytype']['daytypetype'][j].push(0);
-                            }
-                        }
-                        var p = chartdata['daytype']['daytypes'].indexOf(daytype);
-                        chartdata['daytype']['daytypetype'][type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(chartdata['daytype']['daytypetype'][i]));
-                }
-                makechart(cols, chartdata['daytype']['daytypes'], null, undefined, null, [], 'category', null);
-                break;
-
-            case "periodHour":
-                if (chartdata['halfhour'] == null) {
-                    chartdata['halfhour'] = {
-                        'halfhours': [],
-                        'halfhourtype': []
-                    }
-                    chartdata['halfhour']['halfhours'] = [];
-                    chartdata['halfhour']['halfhourtype'] = [];
-                    for (i = 0; i < types.length; i++) {
-                        chartdata['halfhour']['halfhourtype'].push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var halfhour = resp[i]['periodHour'];
-                        if (chartdata['halfhour']['halfhours'].indexOf(halfhour) == -1) {
-                            chartdata['halfhour']['halfhours'].push(halfhour)
-                            for (var j = 0; j < types.length; j++) {
-                                chartdata['halfhour']['halfhourtype'][j].push(0);
-                            }
-                        }
-                        var p = chartdata['halfhour']['halfhours'].indexOf(halfhour);
-                        chartdata['halfhour']['halfhourtype'][type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(chartdata['halfhour']['halfhourtype'][i]));
-                }
-                makechart(cols, chartdata['halfhour']['halfhours'], null, undefined, null, [], 'category', null);
-                break;
-
-
-            case "periodTransantiago":
-                if (chartdata['period'] == null) {
-                    chartdata['period'] = {
-                        'periods': [],
-                        'periodtype': []
-                    }
-                    chartdata['period']['periods'] = [];
-                    chartdata['period']['periodtype'] = [];
-                    for (i = 0; i < types.length; i++) {
-                        chartdata['period']['periodtype'].push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var period = resp[i]['periodTransantiago'];
-                        if (chartdata['period']['periods'].indexOf(period) == -1) {
-                            chartdata['period']['periods'].push(period)
-                            for (var j = 0; j < types.length; j++) {
-                                chartdata['period']['periodtype'][j].push(0);
-                            }
-                        }
-                        var p = chartdata['period']['periods'].indexOf(period);
-                        chartdata['period']['periodtype'][type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(chartdata['period']['periodtype'][i]));
-                }
-                makechart(cols, chartdata['period']['periods'], null, undefined, null, [], 'category', null);
-                break;
-
-
-            case "plate":
-                if (chartdata['plate'] == null) {
-                    chartdata['plate'] = {
-                        'plates': [],
-                        'platetype': []
-                    }
-                    chartdata['plate']['plates'] = [];
-                    chartdata['plate']['platetype'] = [];
-                    for (i = 0; i < types.length; i++) {
-                        chartdata['plate']['platetype'].push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var plate = resp[i]['plate'];
-                        if (chartdata['plate']['plates'].indexOf(plate) == -1) {
-                            chartdata['plate']['plates'].push(plate)
-                            for (var j = 0; j < types.length; j++) {
-                                chartdata['plate']['platetype'][j].push(0);
-                            }
-                        }
-                        var p = chartdata['plate']['plates'].indexOf(plate);
-                        chartdata['plate']['platetype'][type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(chartdata['plate']['platetype'][i]));
-                }
-                makechart(cols, chartdata['plate']['plates'], null, undefined, null, [], 'category', null);
-                break;
-
-            case "service":
-                if (chartdata['service'] == null) {
-                    var services = [];
-                    var servicetype = [];
-                    for (i = 0; i < types.length; i++) {
-                        servicetype.push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var service = resp[i]['service'];
-                        if (services.indexOf(service) == -1) {
-                            services.push(service)
-                            for (var j = 0; j < types.length; j++) {
-                                servicetype[j].push(0);
-                            }
-                        }
-                        var p = services.indexOf(service);
-                        servicetype[type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = [];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(servicetype[i]));
-                }
-                makechart(cols, services, null, undefined, null, [], 'category', null);
-                break;
-
-            case "daily":
-                if (chartdata['daily'] == null) {
-                    var days = [];
-                    var daystype = [];
-                    for (i = 0; i < types.length; i++) {
-                        daystype.push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var day = moment(resp[i]['timeCreation'], "DD-MM-YYYY HH:mm:SS").format("DD-MM-YYYY")
-
-                        if (days.indexOf(day) == -1) {
-                            days.push(day)
-                            for (var j = 0; j < types.length; j++) {
-                                daystype[j].push(0);
-                            }
-                        }
-                        var p = days.indexOf(day);
-                        daystype[type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = days;
-                cols.unshift("x");
-                cols = [cols];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(daystype[i]));
-                }
-                makechart(cols, null, null, 'x', '%d-%m-%Y', [types], 'timeseries', '%d-%m-%Y')
-                break;
-            case "monthly":
-                if (chartdata['monthly'] == null) {
-                    var months = [];
-                    var monthstype = [];
-                    for (i = 0; i < types.length; i++) {
-                        monthstype.push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var month = moment(resp[i]['timeCreation'], "DD-MM-YYYY HH:mm:SS").format("MM-YYYY")
-
-                        if (months.indexOf(month) == -1) {
-                            months.push(month)
-                            for (var j = 0; j < types.length; j++) {
-                                monthstype[j].push(0);
-                            }
-                        }
-                        var p = months.indexOf(month);
-                        monthstype[type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = months;
-                cols.unshift("x");
-                cols = [cols];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(monthstype[i]));
-                }
-                makechart(cols, null, null, 'x', '%m-%Y', [types], 'timeseries', '%m-%Y')
-                break;
-            case "yearly":
-                if (chartdata['yearly'] == null) {
-                    var years = [];
-                    var yearstype = [];
-                    for (i = 0; i < types.length; i++) {
-                        yearstype.push([]);
-                    }
-                    for (i = 0; i < resp.length; i++) {
-                        var type = resp[i]['type'];
-                        var year = moment(resp[i]['timeCreation'], "DD-MM-YYYY HH:mm:SS").format("YYYY")
-
-                        if (years.indexOf(year) == -1) {
-                            years.push(year)
-                            for (var j = 0; j < types.length; j++) {
-                                yearstype[j].push(0);
-                            }
-                        }
-                        var p = years.indexOf(year);
-                        yearstype[type][p] += resp[i]['eventConfirm'];
-                    }
-                }
-                var cols = years;
-                cols.unshift("x");
-                cols = [cols];
-                for (i = 0; i < types.length; i++) {
-                    cols.push([types[i]].concat(yearstype[i]));
-                }
-                makechart(cols, null, null, 'x', '%Y', [types], 'timeseries', '%Y')
-                break;
-        }
     }
 }
+
 function makechart(columns, categories, height, x, xformat, groups, type, tickformat) {
     chart = c3.generate({
         size: {
@@ -375,22 +77,6 @@ function makechart(columns, categories, height, x, xformat, groups, type, tickfo
     });
 }
 
-function updateDate(n) {
-    switch (n) {
-        case 1:
-            $('#date_init').data("DateTimePicker").date(moment());
-            $('#date_end').data("DateTimePicker").date(moment());
-            break;
-        case 2:
-            $('#date_init').data("DateTimePicker").date(moment().subtract(1, 'months'));
-            $('#date_end').data("DateTimePicker").date(moment());
-            break;
-        case 3:
-            $('#date_init').data("DateTimePicker").date(moment().subtract(3, 'months'));
-            $('#date_end').data("DateTimePicker").date(moment());
-            break;
-    }
-}
 
 // # http://localhost/carriers/getActiveUsers/?date=2016-10-24
 // # {
@@ -405,64 +91,18 @@ function updateDate(n) {
 // #       ...
 // # ]}
 function myFunction(refresh) {
-    var Dataurl = "http://" + location.host + "/carriers/getDriversData/";
-    var data = {
-        date_init: $('#date_init').data("DateTimePicker").date().format("YYYY-MM-DD"),
-        date_end: $('#date_end').data("DateTimePicker").date().add(1, 'days').format("YYYY-MM-DD"),
-        /*        hour1: $('#hour1').data("DateTimePicker").date().format("HH"),
-         hour2: $('#hour2').data("DateTimePicker").date().format("HH"),
-         minute1: $('#hour1').data("DateTimePicker").date().format("mm"),
-         minute2: $('#hour2').data("DateTimePicker").date().format("mm")*/
+    var url = "http://" + location.host + "/carriers/getActiveUsers/";
+    var url_opts = {
+        date_init: $('#date_filter').data("DateTimePicker").date().format("YYYY-MM-DD")
     };
-    var service = $(".select2_multiple").val();
-    var plate = $(".select2_plate").val();
-    if (service != null) data['service'] = JSON.stringify(service);
-    if (plate != null) data['plate'] = JSON.stringify(plate);
 
-    $.getJSON(Dataurl, data)
+    $.getJSON(url, url_opts)
         .done(function (data) {
-            console.log(data);
             if (refresh) {
-                $(".select2_plate").find("option").remove().val("");
-                $("#plate").val(null).trigger("change");
 
-                $('.select2_plate').append($('<option>', {
-                    value: "Todas las patentes",
-                    text: "Todas las patentes",
-                }));
-                var top = [];
-                var bottom = [];
-                $.each(data.allplates, function (key, value) {
-                    if (value) {
-                        top.push(key);
-                    }
-                    else {
-                        bottom.push(key);
-                    }
-                })
-                top.sort();
-                values = [];
-                $.each(top, function (index, value) {
-                    if ($.inArray( value, plate ) >= 0){
-                        values.push(value);
-                    };
-                    $('.select2_plate').append($('<option>', {
-                        value: value,
-                        text: value,
-                    }));
-                });
-                bottom.sort();
-                $.each(bottom, function (index, value) {
-                    $('.select2_plate').append($('<option>', {
-                        value: value,
-                        text: value + " (No hay datos)",
-                    }));
-                });
-                $("#plate").val(values).trigger("change");
             }
+            console.log(data);
             reloadchart();
-            resp = data.reports;
-            types = data.types;
             updatechart();
         });
 }
