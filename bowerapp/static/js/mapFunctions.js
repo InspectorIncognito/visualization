3738b0f51143f9792697c565ca108523166dcbd5
Felipe Hernández <cephei.1313@gmail.com>
2017-08-30 16:24:29 -0300

    moved bus icon to mapFunctions.js

--- bowerapp/static/js/mapFunctions.js
+++ bowerapp/static/js/mapFunctions.js
@@ -55,6 +55,18 @@ var user_icon = L.icon({
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+// bus marker
+var bus_icon = L.icon({
+    iconUrl: "/static/carrier/images/usuario.png",
+    shadowUrl: null,
+
+    iconSize:     [30, 38], // size of the icon
+    shadowSize:   [ 0,  0], // size of the shadow
+    iconAnchor:   [15, 38], // point of the icon which will correspond to marker"s location
+    shadowAnchor: [ 0,  0],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 var origin_icon = L.icon({
     iconUrl: "/static/carrier/images/circulo_inicio.png",
     shadowUrl: null,
